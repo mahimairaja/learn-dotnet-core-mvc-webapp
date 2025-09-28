87a5188ef1dab8449c647cef8a8e32cd0c70d390
Mahimai Raja J <mahimairaja3@gmail.com>
2025-09-28 17:52:20 -0400

    initalized mvc app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace learn_dotnet_core_mvc_webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
